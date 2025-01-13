1ce69f91ce5ba38247743e25bf88dd41535c9c5c
Patricija Delac <patricija.delac@agiledrop.com>
2025-01-13 23:57:29 +0100

    Set up Sass, path aliases, and add font, images, and base styles

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,23 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@styles': '/src/styles',
+      '@types': '/src/types/',
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: 'dashesOnly', // Ensures class names follow the dashes-only convention
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@styles/lib.scss" as *;`,
+      },
+    },
+    devSourcemap: true, // Enable source maps for CSS
+  },
 });
